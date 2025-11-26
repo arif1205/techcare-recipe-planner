27dc599417c55e5ca0e136726e40affa72eb6c76
arif1205 <imdariful71@gmail.com>
2025-11-26 19:29:48 +0600

    feat: added useApi hook to organise the api hooks

---
+++ src/hooks/api/api.hooks.ts
@@ -0,0 +1,12 @@
+import { useGetRecipesQuery } from "../../store/api/api";
+
+export function useApi() {
+	return {
+		queries: {
+			receipes: {
+				getRecipes: useGetRecipesQuery,
+			},
+		},
+		mutations: {},
+	};
+}
