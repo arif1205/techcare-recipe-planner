97356dc3379a4570eda7e1bfb4c7ad0c3bf863a7
arif1205 <imdariful71@gmail.com>
2025-11-27 05:37:24 +0600

    fix: useMealPlanState data format

--- src/hooks/store/global.store.hooks.ts
+++ src/hooks/store/global.store.hooks.ts
@@ -1,5 +1,6 @@
 import type { RootState } from "@/store/store";
 import { useAppSelector } from "./store.hooks";
+import type { MealPlanRecipe } from "@/types";
 
 export function useGlobalState() {
 	return useAppSelector((state: RootState) => state.global);
@@ -8,20 +9,31 @@ export function useGlobalState() {
 export function useMealPlanState(dateRange?: {
 	startDate: Date;
 	endDate: Date;
-}) {
+}): {
+	mealPlan: Record<string, MealPlanRecipe>;
+	startDate?: Date;
+	endDate?: Date;
+} {
 	const mealPlan = useAppSelector((state: RootState) => state.global.mealPlan);
 
 	if (!dateRange) return { mealPlan };
 
 	const mealPlanDates = Object.keys(mealPlan);
 
-	const mealPlanDatesInRange = mealPlanDates.filter((date) => {
-		const dateObj = new Date(date);
-		return dateObj >= dateRange.startDate && dateObj <= dateRange.endDate;
-	});
+	const mealPlansInRange = mealPlanDates.reduce<Record<string, MealPlanRecipe>>(
+		(acc, date) => {
+			const dateObj = new Date(date);
+			if (dateObj >= dateRange.startDate && dateObj <= dateRange.endDate) {
+				acc[date] = mealPlan[date as keyof typeof mealPlan];
+				return acc;
+			}
+			return acc;
+		},
+		{}
+	);
 
 	return {
-		mealPlan: mealPlanDatesInRange,
+		mealPlan: mealPlansInRange,
 		startDate: dateRange.startDate,
 		endDate: dateRange.endDate,
 	};
